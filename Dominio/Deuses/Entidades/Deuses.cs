8fb595fa0207c98cdad84355be89a60296dfc790
Sl4de <rafaelxavierbraga@gmail.com>
2019-10-21 19:15:57 -0300

    Cria entidade Deus

---
+++ Dominio/Deuses/Entidades/Deuses.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dominio.Deuses.Entidades
+{
+    class Deuses
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string PreRequisito { get; set; } //Classes e Raças permitidas
+        public string Simbolo { get; set; }
+        public string Arma { get; set; }
+        public string ObrigacoesEretricoes { get; set; }
+        public string Poderes { get; set; }
+    }
+}
