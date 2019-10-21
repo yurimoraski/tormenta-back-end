abb09175030c90fa43bafdc5a07d539ad989ef7e
Sl4de <rafaelxavierbraga@gmail.com>
2019-10-21 19:07:14 -0300

    Cria entidade Origem
    Origem mesma coisa que antecendente do D&D 5e

---
+++ Dominio/Origens/Entidades/Origem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dominio.Origens.Entidades
+{
+    class Origem
+    {
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Beneficio{ get; set; }
+    }
+}
